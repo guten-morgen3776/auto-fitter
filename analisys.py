cb4484df871efa3bec827d7d85341ff9d59e1cf6
takatoshi aoki <takatoshi.aoki0116@gmail.com>
2025-11-25 13:19:39 +0900

    main.pyを追加し、LinearModelを使用したデータフィッティングの例を実装

--- analisys.py
+++ analisys.py
@@ -1,2 +1,29 @@
-#こんにちは　これから分析アプリの開発を始めるよ
-print('Lets start!')
+import numpy as np
+from scipy.optimize import curve_fit
+
+class BaseModel:
+    def __init__(self):
+        self.params = None
+        self.k = 0
+        self.x_data = None
+        self.y_data = None
+    
+    def fit(self, x, y):
+        self.x_data = np.array(x)
+        self.y_data = np.array(y)
+        self.params, _ = curve_fit(self.func, self.x_data)
+
+    def get_aic(self):
+        y_pred = self.func(self.x_data, *self.params)
+        rss = np.sum((self.y_data - y_pred) ** 2)
+        n = len(self.y_data)
+        aic = n * np.log(rss / n) + 2 * self.k
+        return aic
+    
+class LinearModel(BaseModel):
+    def __init__(self):
+        super().__init__() #親クラスのinitを呼ぶ
+        self.k = 2
+    
+    def func(self, x, a, b):
+        return a * x + b
---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+from analisys import LinearModel
+
+x = [1, 2, 3, 4, 5]
+y = [7.1, 8.9, 11.2, 12,8, 15.1]
+
+model = LinearModel()
+model.fit(x, y)
+
+print(f'params (a, b): {model.params}')
+print(f'AIC-score: {model.get_aic()}')
